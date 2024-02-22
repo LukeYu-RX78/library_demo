861c3f6a164968f60f076e31cbd18a7f3d81f65e
luke.yu <yangyi.yu.luke@gmail.com>
2024-02-23 01:55:04 +1100

    Init demo project.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,11 +14,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'library_demo' title`, () => {
+/*  it(`should have the 'library_demo' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('library_demo');
-  });
+  });*/
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {AgGridAngular} from "ag-grid-angular";
+import {ColDef} from "ag-grid-community";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    AgGridAngular
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
-  title = 'library_demo';
+  title = 'Transation Table';
+  rowData: any[] = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxster', price: 72000 }
+  ];
+  colDefs: ColDef[]=[
+    {field: 'make'},
+    {field: 'model'},
+    {field: 'price'}
+  ];
 }
